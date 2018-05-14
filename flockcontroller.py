d50e39a349b4d417e0f66bbda54002b5e7069417
jose antonio izquierdo <jizquierdo@osnids.com>
2018-05-14 10:00:27 +0200

    -- flock main --

---
+++ flockcontroller.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# owlh controller
+# v0.0 14.05.18 master@owlh.net
+
+import time
+
+def main():
+    while True:
+        print "hellowl!"
+        time.sleep (30)
+
+main()
