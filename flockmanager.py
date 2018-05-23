7277305d5a796273c58a3a6e8549eebc2f574924
jose antonio izquierdo <jizquierdo@osnids.com>
2018-05-23 10:38:11 +0200

    -- D Day --

--- flockmanager.py
+++ flockmanager.py
@@ -2,10 +2,11 @@ import os.path
 import signal
 
 import flockkiller
-import flockdefs
+#import flockdefs
 import flocklogger 
+import flockconf
 
-
+conf = flockconf.get_item
 flogger = flocklogger.flocklogger
 killer = flockkiller.flockKiller()
 
@@ -14,12 +15,12 @@ def killme():
 
 def registerflock():
     flogger (str(os.getpid()))
-    file(flockdefs.pidfile, 'w').write(str(os.getpid()))
+    file(conf("pidfile"), 'w').write(str(os.getpid()))
 
 def amirunning():
-    if os.path.isfile(flockdefs.pidfile):
+    if os.path.isfile(conf("pidfile")):
         flogger ("I'm running, we don't need two of us, exiting...")
-        flogger ("If you think I'm not running, please check process list and %s" % flockdefs.pidfile)
+        flogger ("If you think I'm not running, please check process list and %s" % conf("pidfile"))
         return True
     registerflock()
     return False
@@ -27,7 +28,7 @@ def amirunning():
 def byebye():
     flogger ("Time to go home. See you soon")
     flocklogger.killflocklogger()
-    os.unlink(flockdefs.pidfile)
+    os.unlink(conf("pidfile"))
 
 # Just as a remainder. If pid file is in place, try to show if the process is running for sure.
 def check_pid(pid):        
